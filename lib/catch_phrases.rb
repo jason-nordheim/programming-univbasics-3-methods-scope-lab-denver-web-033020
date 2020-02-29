3afeed94f0159b5534526d9b645ffb6dd4e5a724
Jason Nordheim <jason.nordheim@gmail.com>
2020-02-29 22:14:12 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,19 @@
+
+
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+
+def all_phrases
+  mario()
+  toadstool()
+  puts "It's Dangerous To Go Alone! Take This."
 end
