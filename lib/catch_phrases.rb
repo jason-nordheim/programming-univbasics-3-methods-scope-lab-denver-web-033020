aefbeaf75c86c643ea09bc6b174ca5eb080c8d38
Jason Nordheim <jason.nordheim@gmail.com>
2020-02-29 22:17:05 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -11,9 +11,14 @@ def toadstool
   puts status
 end
 
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase 
+end 
+
 
 def all_phrases(arg)
   mario()
   toadstool()
-  puts "It's Dangerous To Go Alone! Take This."
+  link() 
 end
